419e3ddeaa1f7fd5cde2cd91456da277be279e4b
GabrielBergoeing <gbergoeing@miuandes.cl>
2024-03-28 23:40:56 -0300

    feat: added primitive structure for about

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/home", to: "static_pages#home"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
   root "static_pages#home"
 end
